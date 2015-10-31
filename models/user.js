fb1907134846945a6003584e83accce81a240273
Kuti Bence <kuti.bence@gmail.com>
2015-10-31 11:01:12 +0100

    bead

---
+++ models/user.js
@@ -0,0 +1,42 @@
+var Waterline = require('waterline');
+var bcrypt = require('bcryptjs');
+
+module.exports = Waterline.Collection.extend({
+    identity: 'user',
+    connection: 'disk',
+    attributes: {
+        csaladtag: {
+            type: 'string',
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: 'string',
+            required: true,
+        },
+        surname: {
+            type: 'string',
+            required: true,
+        },
+        forename: {
+            type: 'string',
+            required: true,
+        },
+        todos: {
+            collection: 'todo',
+            via: 'user'
+        },
+        validPassword: function (password) {
+            return bcrypt.compareSync(password, this.password);
+        }
+    },
+    beforeCreate: function(values, next) {
+        bcrypt.hash(values.password, 10, function(err, hash) {
+            if (err) {
+                return next(err);
+            }
+            values.password = hash;
+            next();
+        });
+    }
+});
